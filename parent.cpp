9e28d908d88e372101e50cbde34e4036d6bc13e3
Grzegorz Krajewski <krajekg@gmail.com>
2015-12-04 20:21:13 +0100

    Implement static foo.

--- parent.cpp
+++ parent.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 #include "parent.hpp"
-#include "stacktrace.hpp"
+#include "backtrace.hpp"
 
 Parent::Parent()
 {
@@ -13,8 +13,13 @@ void Parent::PublicParentFoo()
   ProtectedParentFoo();
 }
 
+void Parent::PublicStaticFoo()
+{
+  Backtrace bt;
+  std::cout << bt.ToString() << std::endl;
+}
+
 void Parent::ProtectedParentFoo()
 {
-  Stacktrace st;
-  std::cout << st.ToString() << std::endl;
+  PublicStaticFoo();
 }
