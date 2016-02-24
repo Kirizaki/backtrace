0124acbb7ab151121052c0eb1c3d618fb997dc11
krajekg@gmail.com <krajekg@gmail.com>
2016-02-24 12:32:32 +0100

    Few functions more to test.

---
+++ t.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+
+int main()
+{
+  std::string str = "";
+  
+ str = "1";
+ 
+ if(str != "-1")
+   std::cout << "EQUAL!!!!!!" << std::endl;
+ else
+  {}
+  
+  return 0;
+}
