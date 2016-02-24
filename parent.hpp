4ee565c8e7f06be349555281207544062061a18b
krajekg@gmail.com <krajekg@gmail.com>
2016-02-24 12:38:05 +0100

    Example of use ```mlpack::backtrace```.

---
+++ parent.hpp
@@ -0,0 +1,14 @@
+class Parent
+{
+ public:
+  Parent();
+  
+  void PublicParentFoo();
+  static void PublicStaticFoo();
+  
+ private:
+  void PrivateParentFoo();
+   
+ protected:
+  void ProtectedParentFoo();
+};
